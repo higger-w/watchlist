757bb11907fbf212067aba09f5dcb7e8f092fcec
higger-w <higgerw0039@126.com>
2022-07-29 21:14:37 +0800

    Add static files

---
+++ app.py
@@ -0,0 +1,41 @@
+from flask import Flask,url_for,render_template
+
+name = 'Grey Li'
+movies = [
+    {'title': 'My Neighbor Totoro', 'year': '1989'},
+    {'title': 'Dead Poets Society', 'year': '1989'},
+    {'title': 'A Perfect World', 'year': '1993'},
+    {'title': 'Leon', 'year': '1994'},
+    {'title': 'Mahjong', 'year': '1996'},
+    {'title': 'Swallowtail Butterfly', 'year': '1996'},
+    {'title': 'King of Comedy', 'year': '1999'},
+    {'title': 'Devils on the Doorstep', 'year': '1999'},
+    {'title': 'WALL-E', 'year': '2008'},
+    {'title': 'The Pork of Music', 'year': '2012'},
+]
+
+app = Flask(__name__)
+
+@app.route('/')
+@app.route('/index')
+def index():
+    #return '<h1>Hello Bob!</h1><img src="http://helloflask.com/totoro.gif">'
+    return render_template('index.html', name=name, movies=movies)
+
+@app.route('/colors/<any(blue,red,white):color>')
+def three_color(color):
+    return "<p>There is no problem.</p>"
+
+@app.route('/user/<name>')
+def user_page(name):
+    return 'User: %s'%name
+
+@app.route('/test')
+def test_url_for():
+    print(url_for('hello'))
+    print(url_for('user_page',name='greyli'))
+    print(url_for('user_page',name='peter'))
+    print(url_for('test_url_for'))
+    print(url_for('test_url_for', num=2))
+    return 'Test Page'
+
